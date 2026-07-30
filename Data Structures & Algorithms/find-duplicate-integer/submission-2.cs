e2f48af7f0f1ea6cd0bbccc018c4f96ecae0b395
sakshamdani <71568789+sakshamdani1402@users.noreply.github.com>
2026-07-30 20:16:39 +0530

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int FindDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        while(true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast) {
+                break;
+            }
+        }
+
+        slow = 0;
+        while(true) {
+            slow = nums[slow];
+            fast = nums[fast];
+
+            if(slow == fast) {
+                return slow;
+            }
+        }
+        return -1;
+    }
+}
